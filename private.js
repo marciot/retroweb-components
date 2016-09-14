12482cac8155147098aefaec758bcf84566b5622
Marcio T <mlt4356-github@yahoo.com>
2016-09-14 12:42:28 -0600

    Added Peerjs API key

---
+++ private.js
@@ -0,0 +1,4 @@
+(function(namespace, thisDoc){
+	/* Please replace this with your own PeerJS Cloud API key from http://peerjs.com! */
+	namespace.peerJSConfig = {key: 'u7htss9n8pz257b9'}
+})(window.RetroWeb = window.RetroWeb || {});
